95298958c394a04e82de12627eea2bb1fa954b97
ChaRed03 <93855784+ChaRed03@users.noreply.github.com>
2021-11-28 20:50:30 +0700

    Add files via upload

---
+++ Домашка/2 задание/2.5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int main(){
+    int m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, a;
+    printf("Mecyac:\n");
+    scanf("%d", &a);
+    m1 = 25;
+    m2 = 22;
+    m3 = 29;
+    m4 = 26;
+    m5 = 31;
+    m6 = 28;
+    m7 = 26;
+    m8 = 28;
+    m9 = 27;
+    m10 = 25;
+    m11 = 29;
+    m12 = 27;
+    if (a==1) {
+        printf("%d", m1);
+             }
+    if (a==2) {
+            printf("%d", m2);
+                 }
+    if (a==3) {
+        printf("%d", m3);
+             }
+    if (a==4) {
+        printf("%d", m4);
+             }
+    if (a==5) {
+        printf("%d", m5);
+             }
+    if (a==6) {
+        printf("%d", m6);
+             }
+    if (a==7) {
+        printf("%d", m7);
+             }
+    if (a==8) {
+        printf("%d", m8);
+             }
+    if (a==9) {
+        printf("%d", m9);
+             }
+    if (a==10) {
+        printf("%d", m10);
+             }
+    if (a==11) {
+        printf("%d", m11);
+             }
+    if (a==12) {
+        printf("%d", m12);
+             }
+}
