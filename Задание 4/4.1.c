c647e3322a9661f19616474dde632cb7c7eb79f4
ChaRed03 <93855784+ChaRed03@users.noreply.github.com>
2022-01-02 23:46:43 +0700

    Add files via upload

---
+++ Задание 4/4.1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+   unsigned long long b, d , i;
+   int a;
+    b = 1;
+    scanf("%d", &a);
+    if (a >= 20){
+        printf("00\n");
+        return 0;
+    }
+    while (a >= 1){
+        b = a * b;
+        a = a - 1;
+    }
+    d = b;
+    printf("%llu", d);
+    printf("\n");
+
+    d = d % 100;
+    if (d < 10){
+    printf("0%llu", d);
+    }
+    else{
+        printf("%llu", d);
+    }
+    return 0;
+    }
